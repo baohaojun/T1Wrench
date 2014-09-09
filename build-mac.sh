5c86989d7572d585b5547ebf41329e74a5af6683
Bao Haojun <baohaojun@gmail.com>
2014-09-09 23:22:19 +0800

    mac version
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- build-mac.sh
+++ build-mac.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-qmake
-make -j8
+if test $(uname) = Linux; then
+    psync bhj-mac .
+    remote-cmd bhj-mac bash -c "cd $(up .); ./build-mac.sh"
+    rsync bhj-mac:$(up .)/T1Wrench.app ../t1wrench-macos/ -av
+    rsync bhj-mac:$(up .)/T1Wrench.dmg ~/today/T1Wrench.dmg
+    smb_push ~/today/T1Wrench.dmg
+else
+    rm T1Wrench.app -rf
+    if test ! -d ~/Qt5 -a -d ~/Qt5.bak; then
+        mv ~/Qt5.bak ~/Qt5
+    fi
+    qmake
+    make -j8
+    cp emacs-weixin.sh T1Wrench.app/Contents/MacOS/
+    cp ~/adb T1Wrench.app/Contents/MacOS/
+    rm T1Wrench.dmg -f
+    macdeployqt T1Wrench.app -dmg -verbose=1
+    mv ~/Qt5 ~/Qt5.bak
+fi
